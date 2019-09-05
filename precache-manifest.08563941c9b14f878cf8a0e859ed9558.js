7bca915e950d20beba95a8ef452248520fa03ddf
AlekseyGaikevich <alekseygaikevich@gmail.com>
2019-09-05 16:01:44 +0300

    Updates

---
+++ precache-manifest.08563941c9b14f878cf8a0e859ed9558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4222eceac82afdf4547d6455e087017",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7e9a3fdc89476ea17592",
+    "url": "/portfolio/static/css/main.73aec402.chunk.css"
+  },
+  {
+    "revision": "696497bccc0ee16ecded",
+    "url": "/portfolio/static/js/2.bb70fe2b.chunk.js"
+  },
+  {
+    "revision": "7e9a3fdc89476ea17592",
+    "url": "/portfolio/static/js/main.eb52dff8.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "e7c6feeb2abb09690000bb656371dc0d",
+    "url": "/portfolio/static/media/myfoto.e7c6feeb.jpg"
+  }
+]);
